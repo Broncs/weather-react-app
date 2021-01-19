b5cb4f7304de24832f1ca8f193d60b318515faa3
broncs <jhowfrombrazil@gmail.com>
2021-01-19 09:16:31 -0300

    adding humidity, wind , and temperature

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,32 @@
 import React, { useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext";
+import humidade from "../assets/humidity.svg";
+import vento from "../assets/wind.svg";
+import fellslike from "../assets/temperature.svg";
 
 const Weather = () => {
   const { state } = useContext(WeatherContext);
 
   return (
     <div className="weather-box">
-      <div className="temp">{Math.round(state.weather.main.temp)}°C</div>
+      <div className="temp">
+        {Math.round(state.weather.main.temp)}°C
+        <div className="temp-status">
+          <div className="icons-temp">
+            <img src={humidade} alt="humidity" />
+            <p>{state.weather.main.humidity}%</p>
+          </div>
+          <div className="icons-temp">
+            <img src={vento} alt="wind" />
+            <p>{state.weather.wind.speed} km/h</p>
+          </div>
+          <div className="icons-temp">
+            <img src={fellslike} alt="fellslike" />
+            <p>{state.weather.main.feels_like}°</p>
+          </div>
+        </div>
+      </div>
+
       <div className="weather">{state.weather.weather[0].description}</div>
       <div className="icon">
         <img
